d122f00df7b58787dd6bfbd206a641a7e454b98e
Janne Kujanpää <jikuja@iki.fi>
2016-10-27 00:31:41 +0300

    Initital about page

--- src/main/java/io/github/jikuja/vaadin_yamapa/ui/views/About.java
+++ src/main/java/io/github/jikuja/vaadin_yamapa/ui/views/About.java
@@ -4,12 +4,14 @@ import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.RichTextArea;
+import org.vaadin.viritin.label.RichText;
 
 public class About extends CssLayout implements View {
     public static final String NAME = "about";
 
     public About() {
-        Label about = new Label("About");
+        Label about = new RichText().withMarkDownResource("/ABOUT.md");
         addComponent(about);
     }
 
